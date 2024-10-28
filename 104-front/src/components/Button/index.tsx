912e9947db18cfa298639592dc72af560c270b95
배준형 <wnsgud0895@gmail.com>
2024-10-28 16:57:29 +0900

    feat: 버튼 컴포넌트 className props 추가

--- 104-front/src/components/Button/index.tsx
+++ 104-front/src/components/Button/index.tsx
@@ -3,11 +3,15 @@ import { MouseEventHandler, ReactNode } from 'react'
 interface ButtonProps {
   children: ReactNode
   onClick: MouseEventHandler<HTMLButtonElement>
+  className?: string
 }
 
-const Button = ({ children, onClick }: ButtonProps) => {
-  return <button onClick={onClick}>{children}</button>
+const Button = ({ children, onClick, className }: ButtonProps) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
- 
